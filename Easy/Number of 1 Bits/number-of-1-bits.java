b1b68b69386a12533df99c16aa47cb15557915a5
Arun  Kumar <107796370+Arunkumarxx@users.noreply.github.com>
2024-05-10 15:22:47 +0530

    Updated Solution - GfG to GitHub

--- Easy/Number of 1 Bits/number-of-1-bits.java
+++ Easy/Number of 1 Bits/number-of-1-bits.java
@@ -24,12 +24,14 @@ class GFG {
 class Solution {
     static int setBits(int N) {
         // code here
-     int count=0;
-     while(N>0){
-         N=N&(N-1);
-         count++;
-     }
-     return count;
-     //jhg
+        int count=0;
+        while(N>0)
+        {
+            if((N & 1)==1)
+            count++;
+            
+            N=N>>>1;
+        }
+        return count;
     }
 }
